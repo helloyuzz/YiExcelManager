ab27d4d182295bf3b9fab215aede6b554a5ac1b3
wgrib2.com <yudz@wgrib2.com>
2025-07-18 11:06:39 +0800

    Update ExcelSheetPanel.Designer.cs

--- ExcelSheetPanel.Designer.cs
+++ ExcelSheetPanel.Designer.cs
@@ -23,11 +23,18 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
+            this.components = new System.ComponentModel.Container();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.linkLabel1 = new System.Windows.Forms.LinkLabel();
-            this.label1 = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolZoomCheckBox = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btn_Setting = new System.Windows.Forms.Button();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
             this.panel1.SuspendLayout();
+            this.contextMenuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // listBox1
@@ -38,40 +45,81 @@
             this.listBox1.ItemHeight = 32;
             this.listBox1.Location = new System.Drawing.Point(0, 0);
             this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(356, 422);
+            this.listBox1.Size = new System.Drawing.Size(356, 411);
             this.listBox1.TabIndex = 0;
             this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.linkLabel1);
+            this.panel1.Controls.Add(this.btn_Setting);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
-            this.panel1.Location = new System.Drawing.Point(0, 422);
+            this.panel1.Location = new System.Drawing.Point(0, 411);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(356, 30);
+            this.panel1.Padding = new System.Windows.Forms.Padding(3);
+            this.panel1.Size = new System.Drawing.Size(356, 41);
             this.panel1.TabIndex = 1;
             // 
             // linkLabel1
             // 
             this.linkLabel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.linkLabel1.Location = new System.Drawing.Point(0, 0);
+            this.linkLabel1.Location = new System.Drawing.Point(3, 3);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(356, 30);
+            this.linkLabel1.Size = new System.Drawing.Size(259, 35);
             this.linkLabel1.TabIndex = 0;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Author:Yulinyi";
             this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
-            // label1
+            // contextMenuStrip1
             // 
-            this.label1.Dock = System.Windows.Forms.DockStyle.Right;
-            this.label1.Location = new System.Drawing.Point(280, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 30);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "V1.0";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.contextMenuStrip1.AutoSize = false;
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolZoomCheckBox,
+            this.toolStripSeparator1,
+            this.toolStripMenuItem1,
+            this.toolStripMenuItem2});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.ShowCheckMargin = true;
+            this.contextMenuStrip1.Size = new System.Drawing.Size(274, 156);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(273, 36);
+            this.toolStripMenuItem1.Text = "版本V1.0";
+            // 
+            // toolZoomCheckBox
+            // 
+            this.toolZoomCheckBox.Checked = true;
+            this.toolZoomCheckBox.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.toolZoomCheckBox.Name = "toolZoomCheckBox";
+            this.toolZoomCheckBox.Size = new System.Drawing.Size(295, 36);
+            this.toolZoomCheckBox.Text = "缩放至1:1（100%）";
+            this.toolZoomCheckBox.Click += new System.EventHandler(this.toolZoomCheckBox_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(270, 6);
+            // 
+            // btn_Setting
+            // 
+            this.btn_Setting.Dock = System.Windows.Forms.DockStyle.Right;
+            this.btn_Setting.Location = new System.Drawing.Point(262, 3);
+            this.btn_Setting.Name = "btn_Setting";
+            this.btn_Setting.Size = new System.Drawing.Size(91, 35);
+            this.btn_Setting.TabIndex = 1;
+            this.btn_Setting.Text = "设置";
+            this.btn_Setting.UseVisualStyleBackColor = true;
+            this.btn_Setting.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btn_Setting_MouseClick);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(273, 36);
+            this.toolStripMenuItem2.Text = "检查更新";
             // 
             // ExcelSheetPanel
             // 
@@ -82,6 +130,7 @@
             this.Name = "ExcelSheetPanel";
             this.Size = new System.Drawing.Size(356, 452);
             this.panel1.ResumeLayout(false);
+            this.contextMenuStrip1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -90,7 +139,12 @@
 
         private System.Windows.Forms.ListBox listBox1;
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolZoomCheckBox;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
+        private System.Windows.Forms.Button btn_Setting;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
     }
 }
